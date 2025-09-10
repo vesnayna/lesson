e40b09da46e1a3d23abf53ed0c7314c2ab09f789
vesnayna <vesnyanavlasova@yandex.ru>
2025-09-10 06:17:45 +0400

    Add files via upload

--- lesson_1_task_3.py
+++ lesson_1_task_3.py
@@ -1,3 +1,3 @@
-first_name = ("Введите ваше имя: ")
-last_name = ("Введите вашу фамилию: ")
-print("Вас зовут:" + first_name + last_name) 
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
+print(f"Вас зовут: {last_name} {first_name}")
--- lesson_1_task_5.py
+++ lesson_1_task_5.py
@@ -1,14 +1,14 @@
-def print_number(num):
+def print_num(num):
     print(num, end='')
-
-print_number (8)
-print_number (8)
-print_number (0)
-print_number (0)
-print_number (5)
-print_number (5)
-print_number (5)
-print_number (3)
-print_number (5)
-print_number (3)
-print_number (5)
+# Вызов функции 11 раз для вывода номера 88005553535
+print_num(8)
+print_num(8)
+print_num(0)
+print_num(0)
+print_num(5)
+print_num(5)
+print_num(5)
+print_num(3)
+print_num(5)
+print_num(3)
+print_num(5)
